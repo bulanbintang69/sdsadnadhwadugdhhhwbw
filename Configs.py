7806f9588288ec71f8ce06a2d955430bd6f9af2c
hello universe <87313549+bulanbintang69@users.noreply.github.com>
2022-03-04 03:54:51 +0700

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -33,10 +33,10 @@ class Var:
     # Custom Text format to be used, while sending Tweets.
     CUSTOM_TEXT = config("CUSTOM_TEXT", None)
     # Text to Display on Button, Attached to Message Posted on Telegram.
-    BUTTON_TITLE = config("BUTTON_TITLE", "View on Twitter🔗")
+    BUTTON_TITLE = config("BUTTON_TITLE", "WIBU JUGA MANUSIA")
     # Set DISABLE_BUTTON to True, to disable that Button.
     CUSTOM_BUTTON = config("CUSTOM_BUTTON", default=None)
-    DISABLE_BUTTON = config("DISABLE_BUTTON", default=False, cast=bool)
+    DISABLE_BUTTON = config("DISABLE_BUTTON", default=True)
 
     # Media Url, to be send with '/start' message.
     START_MEDIA = config("START_MEDIA", "TgTwitterStreamer/assets/START.webp")
@@ -49,7 +49,7 @@ class Var:
     # Whether should take messages, which are reply to other post.
     TAKE_REPLIES = config("TAKE_REPLIES", default=False, cast=bool)
     # Whether to Take Retweets or not.
-    TAKE_RETWEETS = config("TAKE_RETWEETS", default=False, cast=bool)
+    TAKE_RETWEETS = config("TAKE_RETWEETS", default=True, cast=bool)
     # Whether to take replies on post of user filled in TRACK_USERS.
     TAKE_OTHERS_REPLY = config("TAKE_OTHERS_REPLY", default=False, cast=bool)
 
@@ -58,8 +58,8 @@ class Var:
     MUST_EXCLUDE = config("MUST_EXCLUDE", default=None)
 
     # Automations
-    AUTO_LIKE = config("AUTO_LIKE", default=False, cast=bool)
-    AUTO_RETWEET = config("AUTO_RETWEET", default=False, cast=bool)
+    AUTO_LIKE = config("AUTO_LIKE", default=True, cast=bool)
+    AUTO_RETWEET = config("AUTO_RETWEET", default=True, cast=bool)
     AUTO_PIN = config("AUTO_PIN", default=False, cast=bool)
 
     _filter_level = None
